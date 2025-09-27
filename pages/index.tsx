d94b5af8a435c3c6029795bef67941195f7aeaf8
vanminhquangtri <vanminhquangtri@gmail.com>
2025-09-27 17:06:30 +0700

    input

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <header className="fixed w-full left-0 top-0 h-[80px] z-[10] flex flex-col justify-center duration-800 bg-red-800 text-white font-bold text-[18px] header">
-        New Header
+        input
       </header>
 
       <div>
@@ -16,7 +16,7 @@ export default function Home() {
       </div>
 
       <div>
-        <textarea className="bg-black w-full auto-expand-textarea resize-none chat-input flex-1 font-[400] text-[16px] p-6 outline-none text-white caret-[#FF6500] flex items-center"></textarea>
+        <input className="bg-black w-full auto-expand-textarea resize-none chat-input flex-1 font-[400] text-[16px] p-6 outline-none text-white caret-[#FF6500] flex items-center"></input>
       </div>
     </div>
   );
