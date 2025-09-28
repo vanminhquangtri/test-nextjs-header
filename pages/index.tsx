03b75c187626f2d0cdeda9e7a18ed9f5b8fdc84b
vanminhquangtri <vanminhquangtri@gmail.com>
2025-09-28 11:06:52 +0700

    handleFocus

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
+import { useRef } from "react";
+
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleFocus = () => {
+    document.body.classList.add("overflow-hidden");
+  };
+  const handleBlur = () => {
+    document.body.classList.remove("overflow-hidden");
+  };
   return (
     <div className="h-screen overflow-auto">
       <header className="fixed w-full left-0 top-0 h-[80px] z-[10] flex flex-col justify-center duration-800 bg-red-800 text-white font-bold text-[18px] header">
-        overflow
+        handleFocus
       </header>
 
       <div>
@@ -54,7 +63,12 @@ export default function Home() {
         asperiores dolorum possimus ratione fuga! Et in earum illo?
       </div>
       <div>
-        <input className="bg-black w-full auto-expand-textarea resize-none chat-input flex-1 font-[400] text-[16px] p-6 outline-none text-white caret-[#FF6500] flex items-center"></input>
+        <input
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          ref={inputRef}
+          className="bg-black w-full auto-expand-textarea resize-none chat-input flex-1 font-[400] text-[16px] p-6 outline-none text-white caret-[#FF6500] flex items-center"
+        ></input>
       </div>
       <div>
         {Array.from({ length: 40 }, (_, i) => (
