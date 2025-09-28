759328d9a38b8221368f3663e3a62717f9131a96
vanminhquangtri <vanminhquangtri@gmail.com>
2025-09-28 11:08:08 +0700

    5

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,57 +11,27 @@ export default function Home() {
   return (
     <div className="h-screen overflow-auto">
       <header className="fixed w-full left-0 top-0 h-[80px] z-[10] flex flex-col justify-center duration-800 bg-red-800 text-white font-bold text-[18px] header">
-        handleFocus
+        5
       </header>
 
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ipsum
-        mollitia iusto neque similique hic magnam? Vitae nobis magnam
-        consequuntur, ratione tempora enim quo minus distinctio, nostrum minima,
-        aut perspiciatis.
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A est ab
-        suscipit. Et, dignissimos. Eos tenetur magnam vel quos minima tempore
-        asperiores dolorum possimus ratione fuga! Et in earum illo?
-      </div>
+      {Array.from({ length: 3 }, (_, i) => (
+        <div key={i}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ea
+          dolor a nam fugiat neque sunt, esse, deleniti et minima rem voluptas
+          distinctio ducimus quia magnam modi reiciendis animi porro.
+        </div>
+      ))}
 
       <div>
         <input className="bg-black w-full auto-expand-textarea resize-none chat-input flex-1 font-[400] text-[16px] p-6 outline-none text-white caret-[#FF6500] flex items-center"></input>
       </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ipsum
-        mollitia iusto neque similique hic magnam? Vitae nobis magnam
-        consequuntur, ratione tempora enim quo minus distinctio, nostrum minima,
-        aut perspiciatis.
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A est ab
-        suscipit. Et, dignissimos. Eos tenetur magnam vel quos minima tempore
-        asperiores dolorum possimus ratione fuga! Et in earum illo?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ipsum
-        mollitia iusto neque similique hic magnam? Vitae nobis magnam
-        consequuntur, ratione tempora enim quo minus distinctio, nostrum minima,
-        aut perspiciatis.
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A est ab
-        suscipit. Et, dignissimos. Eos tenetur magnam vel quos minima tempore
-        asperiores dolorum possimus ratione fuga! Et in earum illo?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ipsum
-        mollitia iusto neque similique hic magnam? Vitae nobis magnam
-        consequuntur, ratione tempora enim quo minus distinctio, nostrum minima,
-        aut perspiciatis.
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A est ab
-        suscipit. Et, dignissimos. Eos tenetur magnam vel quos minima tempore
-        asperiores dolorum possimus ratione fuga! Et in earum illo?
-      </div>
+      {Array.from({ length: 5 }, (_, i) => (
+        <div key={i}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ea
+          dolor a nam fugiat neque sunt, esse, deleniti et minima rem voluptas
+          distinctio ducimus quia magnam modi reiciendis animi porro.
+        </div>
+      ))}
       <div>
         <input
           onBlur={handleBlur}
